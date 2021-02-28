2d23a59d40082a6bbdf90bc0c78750a00cdba047
Borewit <borewit@users.noreply.github.com>
2021-02-28 18:59:59 +0100

    Borewit/tokenizer-token#2: Replace Buffer with Uint8Array for better cross JavaScript application support

--- index.d.ts
+++ index.d.ts
@@ -11,20 +11,20 @@ export interface IGetToken<T> {
 
   /**
    * Decode value from buffer at offset
-   * @param buffer - Buffer to read the decoded value from
+   * @param array - Uint8Array to read the decoded value from
    * @param offset - Decode offset
    * @return decoded value
    */
-  get(buffer: Buffer, offset: number): T;
+  get(array: Uint8Array, offset: number): T;
 }
 
 export interface IToken<T> extends IGetToken<T> {
   /**
    * Encode value to buffer
-   * @param buffer - Buffer to write the encoded value to
+   * @param array - Uint8Array to write the encoded value to
    * @param offset - Buffer write offset
    * @param value - Value to decode of type T
    * @return offset plus number of bytes written
    */
-  put(buffer: Buffer, offset: number, value: T): number
+  put(array: Uint8Array, offset: number, value: T): number
 }
